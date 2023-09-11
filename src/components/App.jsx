215c0205969778fb2ac3e843b51eac4be1fe49c7
SJanna <jannas@uninorte.edu.co>
2023-09-11 18:15:07 -0400

    Button Y Button Group

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,30 +4,45 @@ import ItemUser from "./ItemUser";
 // import UserCount from "./UserCount";
 import UserForm from "./UserForm";
 import userList from "../data";
+import { Button, Box, ButtonGroup } from "@mui/material";
 
 export class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      users:userList,
-    }
+      users: userList,
+    };
   }
 
   handleChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
-  })
+    });
   };
 
-  render() {         
+  render() {
     return (
       <>
         <Title text="Lista de Usuarios" />
-        {/* {this.state.users.map((user, index) => (
-          <ItemUser key={index} user={user}/>
-        ))} */}
-        <UserForm onChange={this.handleChange} user={this.state}/>
+        {this.state.users.map((user, index) => (
+          <ItemUser key={index} user={user} />
+        ))}
+        {/* <UserForm onChange={this.handleChange} user={this.state}/> */}
+        <Box align="center">
+          <Button color="secondary" variant="contained">
+            Cargar más
+          </Button>
+        </Box>
+        <Box align="center">
+          <ButtonGroup
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button color="primary">Anterior</Button>
+            <Button color="primary">Siguiente</Button>
+          </ButtonGroup>
+        </Box>
       </>
     );
   }
