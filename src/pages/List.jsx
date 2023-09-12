d93663496d9ab43b58e1e6b8bb28af1a40d33849
SJanna <jannas@uninorte.edu.co>
2023-09-12 05:43:04 -0400

    Solicitando datos (GET)

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,19 +1,32 @@
-import React from "react";
-import userList from "../data.js";
+import React, { useEffect, useState } from "react";
 import Title from "../components/Title.jsx";
 import ItemUser from "../components/ItemUser";
+import axios from "axios";
 
 const List = () => {
-  const users = userList;
+  const [users, setUsers] = useState([]);
 
-    return(
-      <>
-      <Title text="Lista de usuarios"/>
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/users");
+      setUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <Title text="Lista de usuarios" />
       {users.map((item, index) => (
-        <ItemUser key={index} user={item}/>
+        <ItemUser key={index} user={item} />
       ))}
-      </>
-    )
-  }
+    </>
+  );
+};
 
 export default List;
