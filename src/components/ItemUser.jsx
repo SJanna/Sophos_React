3567ad27f1716341dff54af19072f28d6e942bd0
SJanna <jannas@uninorte.edu.co>
2023-09-11 22:19:07 -0400

    Agregando estilos personalizados Parte I

--- src/components/ItemUser.jsx
+++ src/components/ItemUser.jsx
@@ -2,33 +2,47 @@ import React from "react";
 import "./styles/ItemUser.css";
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
-export default class ItemUser extends React.Component {
-  state = {};
-  render() {
-    const { nombre, pais, email, telefono } = this.props.user;
-    return (
-      <Card>
-        <CardMedia component="img" img="/image.png" title="Imagen de usuario"/>
-        <CardContent>
-        <Typography variant="h5">
-          <strong>Nombre: {nombre}</strong>
+const ItemUser = (props) => {
+  const { nombre, pais, email, telefono } = props.user;
+  return (
+    <Card
+      sx={{
+        maxWidth: 500,
+        margin: "8px auto",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <CardMedia
+        sx={{
+          flex: 1,
+          maxWidth: 200,
+          padding: "8px",
+          
+        }}
+        component="img"
+        src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+        title="Imagen de usuario"
+      />
+      <CardContent sx={{ flex: 2 }}>
+        <Typography variant="body1">
+          <strong>Nombre: </strong> {nombre}
         </Typography>
-        <Typography variant="h5">
-          <strong>Pais: {pais}</strong>
+        <Typography variant="body1">
+          <strong>Pais: </strong> {pais}
         </Typography>
-        <Typography variant="h5">
-          <strong>Correo: {email}</strong>
-        </Typography >
+        <Typography variant="body1">
+          <strong>Correo: </strong> {email}
+        </Typography>
         {telefono && (
-          <Typography variant="h5">
-            <strong>Teléfono: {telefono}</strong>
+          <Typography variant="body1">
+            <strong>Teléfono: </strong> {telefono}
           </Typography>
         )}
-        </CardContent>
+      </CardContent>
 
-        {this.props.enableButton && <button> Haz Clic </button>}
-      </Card>
-    );
-  }
-}
-// exports default ItemUser;
+      {props.enableButton && <button> Haz Clic </button>}
+    </Card>
+  );
+};
+export default ItemUser;
